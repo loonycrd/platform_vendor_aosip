c31de365c9e5888a82af0bdad673f91c486f4fa4
Rohit Vashisht <rohitv88cf@gmail.com>
2019-07-11 21:14:51 +0530

    Add additional fonts for lockscreen widgets
    Update and remove italic styles for Google Sans.
    
    Add following new fonts-
    * Fira Sans
    * SamsungOne UI
    * San Francisco
    * Slate
    * Ubuntu
    
    Signed-off-by: Rohit Vashisht <rohitv88cf@gmail.com>

--- config/fonts.mk
+++ config/fonts.mk
@@ -1,10 +1,38 @@
 # Fonts
 PRODUCT_COPY_FILES += \
-    vendor/aosip/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
-    vendor/aosip/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
-    vendor/aosip/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
-    vendor/aosip/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
-    vendor/aosip/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
-    vendor/aosip/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
+    vendor/aosip/fonts/Default-Bold.ttf:system/fonts/Default-Bold.ttf \
+    vendor/aosip/fonts/Default-Medium.ttf:system/fonts/Default-Medium.ttf \
+    vendor/aosip/fonts/Default-Regular.ttf:system/fonts/Default-Regular.ttf \
+    vendor/aosip/fonts/DefaultCondensed-Bold.ttf:system/fonts/DefaultCondensed-Bold.ttf \
+    vendor/aosip/fonts/DefaultCondensed-Regular.ttf:system/fonts/DefaultCondensed-Regular.ttf \
+    vendor/aosip/fonts/Fira-Bold.ttf:system/fonts/Fira-Bold.ttf \
+    vendor/aosip/fonts/Fira-Medium.ttf:system/fonts/Fira-Medium.ttf \
+    vendor/aosip/fonts/Fira-Regular.ttf:system/fonts/Fira-Regular.ttf \
+    vendor/aosip/fonts/FiraCondensed-Bold.ttf:system/fonts/FiraCondensed-Bold.ttf \
+    vendor/aosip/fonts/FiraCondensed-Medium.ttf:system/fonts/FiraCondensed-Medium.ttf \
+    vendor/aosip/fonts/FiraCondensed-Regular.ttf:system/fonts/FiraCondensed-Regular.ttf \
+    vendor/aosip/fonts/GSans-Regular.ttf:system/fonts/GSans-Bold.ttf \
+    vendor/aosip/fonts/GSans-Medium.ttf:system/fonts/GSans-Medium.ttf \
+    vendor/aosip/fonts/GSans-Bold.ttf:system/fonts/GSans-Regular.ttf \
+    vendor/aosip/fonts/GSansCondensed-Bold.ttf:system/fonts/GSansCondensed-Bold.ttf \
+    vendor/aosip/fonts/GSansCondensed-Regular.ttf:system/fonts/GSansCondensed-Regular.ttf \
+    vendor/aosip/fonts/SamOne-Bold.ttf:system/fonts/SamOne-Bold.ttf \
+    vendor/aosip/fonts/SamOne-Medium.ttf:system/fonts/SamOne-Medium.ttf \
+    vendor/aosip/fonts/SamOne-Regular.ttf:system/fonts/SamOne-Regular.ttf \
+    vendor/aosip/fonts/SF-Bold.otf:system/fonts/SF-Bold.otf \
+    vendor/aosip/fonts/SF-Medium.otf:system/fonts/SF-Medium.otf \
+    vendor/aosip/fonts/SF-Regular.otf:system/fonts/SF-Regular.otf \
+    vendor/aosip/fonts/SF-CondensedBold.otf:system/fonts/SF-CondensedBold.otf \
+    vendor/aosip/fonts/SF-CondensedMedium.otf:system/fonts/SF-CondensedMedium.otf \
+    vendor/aosip/fonts/SF-CondensedRegular.otf:system/fonts/SF-CondensedRegular.otf \
+    vendor/aosip/fonts/Slate-Bold.ttf:system/fonts/Slate-Bold.ttf \
+    vendor/aosip/fonts/Slate-Medium.ttf:system/fonts/Slate-Medium.ttf \
+    vendor/aosip/fonts/Slate-Regular.ttf:system/fonts/Slate-Regular.ttf \
+    vendor/aosip/fonts/SlateCondensed-Bold.ttf:system/fonts/SlateCondensed-Bold.ttf \
+    vendor/aosip/fonts/SlateCondensed-Regular.ttf:system/fonts/SlateCondensed-Regular.ttf \
+    vendor/aosip/fonts/Ubuntu-Bold.ttf:system/fonts/Ubuntu-Bold.ttf \
+    vendor/aosip/fonts/Ubuntu-Medium.ttf:system/fonts/Ubuntu-Medium.ttf \
+    vendor/aosip/fonts/Ubuntu-Regular.ttf:system/fonts/Ubuntu-Regular.ttf \
+    vendor/aosip/fonts/UbuntuCondensed-Regular.ttf:system/fonts/UbuntuCondensed-Regular.ttf
 
-ADDITIONAL_FONTS_FILE := vendor/aosip/fonts/google-sans.xml
+ADDITIONAL_FONTS_FILE := vendor/aosip/fonts/lockscreen-widget-fonts.xml
